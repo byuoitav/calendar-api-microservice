bb2320f672263a1f014318ae971d407d9278e82b
Joe <joseph.as.peterson@gmail.com>
2019-09-04 15:24:36 -0600

    Initialize gsuite

---
+++ gsuite/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/byuoitav/common"
+	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/v2/auth"
+)
+
+func main() {
+	port := ":8034"
+	router := common.NewRouter()
+
+	write := router.Group("", auth.AuthorizeRequest("write-state", "room", auth.LookupResourceFromAddress))
+	read := router.Group("", auth.AuthorizeRequest("read-state", "room", auth.LookupResourceFromAddress))
+
+	router.PUT("/log-level/:level", log.SetLogLevel)
+	router.GET("/log-level", log.GetLogLevel)
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
+	}
+
+	router.StartServer(&server)
+}

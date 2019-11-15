e3ff9b143b97e2666be649e4e851b1bbe3c52d1b
Joe <joseph.as.peterson@gmail.com>
2019-11-15 16:02:08 -0700

    Flesh out exchange plugin

--- exchange/helpers/helpers.go
+++ exchange/helpers/helpers.go
@@ -1,33 +1,138 @@
 package helpers
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/byuoitav/calendar-api-microservice/exchange/models"
+	"github.com/byuoitav/common/log"
 )
 
 // GetExchangeEvents gets events from exchange
 func GetExchangeEvents() ([]models.CalendarEvent, error) {
-	//Identify calendar
+	//Get token
+	token, err := GetToken()
+	if err != nil {
+		log.L.Errorf("")
+		return nil, fmt.Errorf("")
+	}
+	bearerToken := "Bearer " + token
+
+	//Todo: Identify proper calendar
+
 	//Get calendar events for the day
 
-	requestURL := "https://graph.microsoft.com/me/calendarView"
+	requestURL := "https://outlook.office.com/api/v2.0/me/{calendarID}/calendarView"
 	client := &http.Client{}
 	request, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		log.L.Errorf("")
+		return nil, fmt.Errorf("")
+	}
+
+	request.Header.Add("Authorization", bearerToken)
+
+	currentTime := time.Now()
+	currentDayBeginning := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location())
+	currentDayEnding := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 23, 59, 59, 0, currentTime.Location())
 
 	query := request.URL.Query()
-	query.Add("startDateTime", "2019-11-15T00:00:00.0000000")
-	query.Add("endDateTime", "2019-11-15T23:59:59.0000000")
+	query.Add("startDateTime", currentDayBeginning.Format("2006-01-02T15:04:05")+".0000000")
+	query.Add("endDateTime", currentDayEnding.Format("2006-01-02T15:04:05")+".0000000")
 	request.URL.RawQuery = query.Encode()
 
-	//Unmarshal into a list of events
-	return nil, nil
+	resp, err := client.Do(request)
+	if err != nil {
+		log.L.Errorf("")
+		return nil, fmt.Errorf("")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.L.Errorf("")
+		return nil, fmt.Errorf("")
+	}
+
+	var respBody []models.ExchangeEvent
+	err = json.Unmarshal(body, &respBody)
+	if err != nil {
+		log.L.Errorf("")
+		return nil, fmt.Errorf("")
+	}
+
+	var events []models.CalendarEvent
+	for _, event := range respBody {
+		events = append(events, models.CalendarEvent{
+			Name:          event.Subject,
+			startDateTime: "",
+			endDateTime:   "",
+		})
+	}
+
+	return events, nil
 }
 
 // SetExchangeEvent sets an event in exchange
 func SetExchangeEvent(event models.CalendarEvent) error {
-	//Identify calendar
+
+	//Todo: Identify proper calendar
+
+	token, err := GetToken()
+	if err != nil {
+		log.L.Errorf("")
+		return fmt.Errorf("")
+	}
+	bearerToken := "Bearer " + token
+
 	//Convert calendar event into exchange event
+	requestBody := models.ExchangeEventRequest{
+		Subject: event.Name,
+		Start: models.ExchangeDate{
+			DateTime: time.Date(event.StartTime).Format("2006-01-02T15:04:05"),
+			TimeZone: "", // Get timezone
+		},
+		End: models.ExchangeDate{
+			DateTime: time.Date(event.EndTime).Format("2006-01-02T15:04:05"),
+			TimeZone: "", // Get timezone
+		},
+	}
+
 	//Send event request
+
+	requestURL := "https://outlook.office.com/api/v2.0/me/calendars/{calendarID}/events"
+	client := &http.Client{}
+	request, err := http.NewRequest(http.MethodPost, requestURL, bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.L.Errorf("")
+		return fmt.Errorf("")
+	}
+
+	request.Header.Add("Authorization", bearerToken)
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.L.Errorf("")
+		return fmt.Errorf("")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.L.Errorf("")
+		return fmt.Errorf("")
+	}
+
+	var respBody string
+	err = json.Unmarshal(body, &respBody)
+	if err != nil {
+		log.L.Errorf("")
+		return fmt.Errorf("")
+	}
+
 	return nil
 }
--- exchange/models/models.go
+++ exchange/models/models.go
@@ -13,3 +13,68 @@ type ExchangeToken struct {
 	ExpireTime int    `json:"expires_in"`
 	Token      string `json:"access_token"`
 }
+
+// ExchangeEvent models an event returned by microsoft exchange service
+type ExchangeEvent struct {
+	ID                         string       `json:"id"`
+	CreatedDateTime            string       `json:"createdDateTime"`
+	LastModifiedDateTime       string       `json:"lastModifiedDateTime"`
+	ChangeKey                  string       `json:"changeKey"`
+	Categories                 string       `json:"categories"`
+	OriginalStartTimeZone      string       `json:"originalStartTimeZone"`
+	OriginalEndTimeZone        string       `json:"originalEndTimeZone"`
+	ICalUID                    string       `json:"iCalUId"`
+	ReminderMinutesBeforeStart int          `json:"reminderMinutesBeforeStart"`
+	IsReminderOn               bool         `json:"isReminderOn"`
+	HasAttachments             bool         `json:"hasAttachments"`
+	Subject                    string       `json:"subject"`
+	BodyPreview                string       `json:"bodyPreview"`
+	Importance                 string       `json:"importance"`
+	Sensitivity                string       `json:"sensitivity"`
+	IsAllDay                   bool         `json:"isAllDay"`
+	IsCancelled                bool         `json:"isCancelled"`
+	IsOrganizer                bool         `json:"isOrganizer"`
+	ResponseRequested          bool         `json:"responseRequested"`
+	SeriesMasterID             string       `json:"seriesMasterId"`
+	ShowAs                     string       `json:"showAs"`
+	EventType                  string       `json:"type"`
+	WebLink                    string       `json:"webLink"`
+	OnlineMeetingURL           string       `json:"onlineMeetingUrl"`
+	Recurrence                 string       `json:"recurrence"`
+	ResponseStatus             string       `json:"responseStatus"`
+	Body                       ExchangeBody `json:"body"`
+	Start                      ExchangeDate `json:"start"`
+	End                        ExchangeDate `json:"end"`
+	Location                   string       `json:"location"`
+	Locations                  string       `json:"locations"`
+	Attendees                  string       `json:"attendees"`
+	Organizer                  string       `json:"organizer"`
+}
+
+type ExchangeEventRequest struct {
+	Subject   string             `json:"subject"`
+	Body      ExchangeBody       `json:"body"`
+	Start     ExchangeDate       `json:"start"`
+	End       ExchangeDate       `json:"end"`
+	Attendees []ExchangeAttendee `json:"attendees"`
+}
+
+type ExchangeBody struct {
+	ContentType string `json:"contentType"`
+	Content     string `json:"content"`
+}
+
+type ExchangeDate struct {
+	DateTime string `json:"dateTime"`
+	TimeZone string `json:"timeZone"`
+}
+
+type ExchangeAttendee struct {
+	EmailAddress ExchangeEmailAddress `json:"emailAddress"`
+	Type         string               `json:"type"`
+}
+
+type ExchangeEmailAddress struct {
+	Address string `json:"address"`
+	Name    string `json:"name"`
+}

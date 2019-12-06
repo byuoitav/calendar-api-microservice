86437bedb907018dd6bf30092b456769d8864060
Joe <joseph.as.peterson@gmail.com>
2019-12-06 14:09:45 -0700

    Allow calling Google, Exchange and TeamUp services

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -14,50 +14,54 @@ import (
 
 //GetEvents gets calendar events from the appropriate microservice
 func GetEvents(roomConfig structs.ScheduleConfig) ([]models.CalendarEvent, error) {
+	serviceURL := "http://localhost"
 	switch roomConfig.CalendarType {
 	case "Google":
 		log.L.Info("Calling G Suite microservice")
 		log.L.Infof("Requesting G Suite events for resource: %s", roomConfig.Name)
-		return getEventsRequest(roomConfig.Resource, "http://localhost:8034/events/")
+		serviceURL += ":8034/events/" + roomConfig.Resource
 	case "Exchange":
 		log.L.Info("Calling Exchange microservice")
-		return nil, fmt.Errorf("Exchange service is currently unavailable")
+		log.L.Infof("Requesting Exchange events for resource: %s", roomConfig.Name)
+		serviceURL += ":8035/events/" + roomConfig.CalendarName + "/" + roomConfig.Resource // Change id to be calendar name
 	case "TeamUp":
 		log.L.Info("Calling TeamUp microservice")
 		log.L.Infof("Requesting TeamUp events for resource: %s", roomConfig.Name)
-		return getEventsRequest(roomConfig.Resource, "http://localhost:8036/events/")
+		serviceURL += ":8036/events" + roomConfig.Resource
 	default:
 		return nil, fmt.Errorf("Device %s currently has no calendar type setting", roomConfig.ID)
 	}
+	return getEventsRequest(serviceURL)
 }
 
 //SetEvent sends a calendar event to the appropriate microservice
 func SetEvent(roomConfig structs.ScheduleConfig, event models.CalendarEvent) error {
+	serviceURL := "http://localhost"
 	switch roomConfig.CalendarType {
 	case "Google":
 		log.L.Info("Calling G Suite microservice")
 		log.L.Infof("Sending G Suite event to calendar for resource: %s", roomConfig.Name)
-		return sendEventsRequest(roomConfig.Resource, event, "http://localhost:8034/events/")
+		serviceURL += ":8034/events/" + roomConfig.Resource
 	case "Exchange":
 		log.L.Info("Calling Exchange microservice")
-		return fmt.Errorf("Exchange service is currently unavailable")
+		serviceURL += ":8035/events/" + roomConfig.CalendarName + "/" + roomConfig.Resource // Change id to be calendar name
 	case "TeamUp":
 		log.L.Info("Calling TeamUp microservice")
 		log.L.Infof("Sending TeamUp event to calendar for resource: %s", roomConfig.Name)
-		return sendEventsRequest(roomConfig.Resource, event, "http://localhost:8036/events/")
+		serviceURL += ":8036/events" + roomConfig.Resource
 	default:
 		return fmt.Errorf("Device %s currently has no calendar type setting", roomConfig.ID)
 	}
+	return sendEventsRequest(serviceURL, event)
 }
 
 //getEventsRequest gets all calendar events from the provided microservice
-func getEventsRequest(room string, serviceURL string) ([]models.CalendarEvent, error) {
-	//Call the gsuite microservice
-	requestURL := serviceURL + room
-	resp, err := http.Get(requestURL)
+func getEventsRequest(serviceURL string) ([]models.CalendarEvent, error) {
+
+	resp, err := http.Get(serviceURL)
 	if err != nil {
-		log.L.Errorf("Cannot send request to: %s | %v", requestURL, err)
-		return nil, fmt.Errorf("Cannot send request to: %s | %v", requestURL, err)
+		log.L.Errorf("Cannot send request to: %s | %v", serviceURL, err)
+		return nil, fmt.Errorf("Cannot send request to: %s | %v", serviceURL, err)
 	}
 	defer resp.Body.Close()
 
@@ -83,9 +87,8 @@ func getEventsRequest(room string, serviceURL string) ([]models.CalendarEvent, e
 }
 
 //sendEventsRequest sends an event object to the provided microservice
-func sendEventsRequest(room string, event models.CalendarEvent, serviceURL string) error {
+func sendEventsRequest(serviceURL string, event models.CalendarEvent) error {
 
-	requestURL := serviceURL + room
 	requestBody, err := json.Marshal(event)
 	if err != nil {
 		log.L.Errorf("Cannot convert event to JSON | %v", err)
@@ -95,17 +98,17 @@ func sendEventsRequest(room string, event models.CalendarEvent, serviceURL strin
 	log.L.Infof("Event object: %s", requestBody)
 
 	client := &http.Client{}
-	request, err := http.NewRequest(http.MethodPut, requestURL, bytes.NewBuffer(requestBody))
+	request, err := http.NewRequest(http.MethodPut, serviceURL, bytes.NewBuffer(requestBody))
 	request.Header.Set("Content-type", "application/json")
 	if err != nil {
-		log.L.Errorf("Cannot make HTTP Put request to: %s | %v", requestURL, err)
-		return fmt.Errorf("Cannot make HTTP Put request to: %s | %v", requestURL, err)
+		log.L.Errorf("Cannot make HTTP Put request to: %s | %v", serviceURL, err)
+		return fmt.Errorf("Cannot make HTTP Put request to: %s | %v", serviceURL, err)
 	}
 
 	resp, err := client.Do(request)
 	if err != nil {
-		log.L.Errorf("Cannot send request to: %s | %v", requestURL, err)
-		return fmt.Errorf("Cannot send request to: %s | %v", requestURL, err)
+		log.L.Errorf("Cannot send request to: %s | %v", serviceURL, err)
+		return fmt.Errorf("Cannot send request to: %s | %v", serviceURL, err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
